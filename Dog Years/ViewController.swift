a8d32a71ecf73f5133282dfd8e7e206f4772d231
Ari Mir <ari@Aris-iMac.local>
2014-09-24 21:28:22 -0700

    scientific calculator

--- Dog Years/ViewController.swift
+++ Dog Years/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var inputDogYearsTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertDogYears(sender: UIButton)
+    {
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(inputDogYearsTextField.text.toInt()! * 7)"
+        inputDogYearsTextField.resignFirstResponder()
+        inputDogYearsTextField.text = ""
+    }
 
+    @IBAction func convertScientificDogYears(sender: UIButton)
+    {
+        dogYearsLabel.hidden = false
+        var dogYears = inputDogYearsTextField.text.toInt()!
+        var convertedDogYears = Double(dogYears)
+        
+        if convertedDogYears <= 2
+        {
+            dogYearsLabel.text = "\(convertedDogYears * 10.5)"
+        }
+        
+        else if convertedDogYears > 2
+        {
+            dogYearsLabel.text = "\(21 + ((convertedDogYears - 2) * 10.5))"
+        }
+        
+        inputDogYearsTextField.resignFirstResponder()
+        inputDogYearsTextField.text = ""
+    }
 }
 
